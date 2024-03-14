9bb320e3a243a38ea63633aa0016cc33044b768d
fanto6314 <antonio.foresta04@gmail.com>
2024-03-14 21:31:25 +0100

    [+] Cors Interceptor

---
+++ src/main/java/co/ethpays/explorer/api/WebConfig.java
@@ -0,0 +1,20 @@
+package co.ethpays.explorer.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "X-Auth-Token", "Authorization")
+                .maxAge(3600); // Optional: set the maximum age (in seconds) of the preflight OPTIONS request
+    }
+}
