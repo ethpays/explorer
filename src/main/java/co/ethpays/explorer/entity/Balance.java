6a05dfeb087a6a5feb18cc163be6c0d463ad5c6c
fanto6314 <antonio.foresta04@gmail.com>
2024-03-14 21:58:48 +0100

    [+] Update

---
+++ src/main/java/co/ethpays/explorer/entity/Balance.java
@@ -0,0 +1,26 @@
+package co.ethpays.explorer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "balance")
+public class Balance {
+    @Id
+    private String address;
+    private String username;
+    private String currency;
+    private String type; //spot, margin, futures
+
+    private double balance; //total
+    private double available; //available
+    private double pending; //in process
+    private double locked; //in trade
+
+    private boolean isSystemAccount; //true if system account
+}
