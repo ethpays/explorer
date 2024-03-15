5bb220f03b86850e780aa0d15dc54291e9054835
fanto6314 <antonio.foresta04@gmail.com>
2024-03-15 09:49:40 +0100

    [+] /{txid} get mapping

--- src/main/java/co/ethpays/explorer/api/controllers/TransactionController.java
+++ src/main/java/co/ethpays/explorer/api/controllers/TransactionController.java
@@ -1,4 +1,38 @@
 package co.ethpays.explorer.api.controllers;
 
+import co.ethpays.explorer.entity.Transaction;
+import co.ethpays.explorer.repositories.TransactionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/transaction")
 public class TransactionController {
+    private final TransactionRepository transactionRepository;
+
+    @GetMapping(value = "/{txid}")
+    public Map<String, String> getTransaction(@PathVariable String txid) {
+        Transaction transaction = transactionRepository.findByTransactionId(txid);
+        if (transaction == null) {
+            return Map.of("error", "Transaction not found", "ok", "false", "txid", txid);
+        } else {
+            Map<String, String> data = new LinkedHashMap<>();
+            data.put("ok", "true");
+            data.put("txid", transaction.getTransactionId());
+            data.put("amount", String.valueOf(transaction.getAmount()));
+            data.put("currency", transaction.getCurrency());
+            data.put("from", transaction.getFromWallet());
+            data.put("to", transaction.getToWallet());
+            data.put("status", transaction.getStatus());
+            data.put("timestamp", String.valueOf(transaction.getCreatedAt().getTime()));
+            return data;
+        }
+    }
 }
