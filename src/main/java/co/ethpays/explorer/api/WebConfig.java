cf18de914ca90737db7ea0937a3440c54585e7b9
fanto6314 <antonio.foresta04@gmail.com>
2024-03-15 09:49:10 +0100

    [+] Url path update

--- src/main/java/co/ethpays/explorer/api/WebConfig.java
+++ src/main/java/co/ethpays/explorer/api/WebConfig.java
@@ -3,6 +3,7 @@ package co.ethpays.explorer.api;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Origin", "Content-Type", "X-Auth-Token", "Authorization")
                 .maxAge(3600); // Optional: set the maximum age (in seconds) of the preflight OPTIONS request
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
     }
 }
